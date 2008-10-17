8f83829dd27d4459699dfd3eb76c7e1b163bae45
jre <jre@email.com>
2008-10-17 10:41:37 +0000

    fix test

--- trust/trunk/test/dk/itst/oiosaml/trust/TokenClientTest.java
+++ trust/trunk/test/dk/itst/oiosaml/trust/TokenClientTest.java
@@ -23,20 +23,25 @@
  */
 package dk.itst.oiosaml.trust;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+
 import org.joda.time.DateTime;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.opensaml.saml2.core.Assertion;
-import org.opensaml.ws.soap.soap11.Envelope;
 import org.opensaml.ws.wsaddressing.Address;
 import org.opensaml.ws.wsaddressing.EndpointReference;
 import org.opensaml.ws.wsaddressing.Metadata;
-import org.opensaml.ws.wssecurity.Security;
-import org.opensaml.xml.XMLObject;
 import org.opensaml.xml.security.x509.BasicX509Credential;
-import org.opensaml.xml.signature.Signature;
-import org.opensaml.xml.signature.SignatureValidator;
+import org.opensaml.xml.signature.KeyInfo;
+import org.opensaml.xml.signature.X509Certificate;
+import org.opensaml.xml.util.Base64;
+import org.opensaml.xml.util.XMLHelper;
+import org.w3c.dom.Element;
 
 import dk.itst.oiosaml.common.SAMLUtil;
 import dk.itst.oiosaml.liberty.SecurityContext;
@@ -86,30 +91,43 @@ public class TokenClientTest extends TrustTests {
 		
 		
 		BasicX509Credential credential = Utils.getCredential("/home/recht/download/TestMOCES1.pfx", "Test1234");
-		TokenClient client = new TokenClient(epr, credential);
+		BasicX509Credential stsCredential = Utils.getCredential("/home/recht/download/TestVOCES1.pfx", "Test1234");
+		TokenClient client = new TokenClient(epr, credential, stsCredential.getPublicKey());
 		client.setAppliesTo("urn:appliesto");
 		
-		String xml = client.toXMLRequest();
-		System.out.println(xml);
-		
-		if (true) return;
-		Envelope env = (Envelope) SAMLUtil.unmarshallElementFromString(xml);
-		for (XMLObject headerObject : env.getHeader().getOrderedChildren()) {
-			if (headerObject == null) continue;
-			if (Security.ELEMENT_NAME.equals(headerObject.getElementQName())) {
-				Signature signature = (Signature) ((Security)headerObject).getUnknownXMLObjects(Signature.DEFAULT_ELEMENT_NAME).get(0);
-				
-				BasicX509Credential credential2 = new BasicX509Credential();
-				credential2.setPublicKey(credential.getPublicKey());
-				SignatureValidator validator = new SignatureValidator(credential);
-				validator.validate(signature);
-				
-				break;
-			}
-		}
+//		String xml = client.toXMLRequest();
+//		System.out.println(xml);
+//		
+//		if (true) return;
+//		Envelope env = (Envelope) SAMLUtil.unmarshallElementFromString(xml);
+//		for (XMLObject headerObject : env.getHeader().getOrderedChildren()) {
+//			if (headerObject == null) continue;
+//			if (Security.ELEMENT_NAME.equals(headerObject.getElementQName())) {
+//				Signature signature = (Signature) ((Security)headerObject).getUnknownXMLObjects(Signature.DEFAULT_ELEMENT_NAME).get(0);
+//				
+//				BasicX509Credential credential2 = new BasicX509Credential();
+//				credential2.setPublicKey(credential.getPublicKey());
+//				SignatureValidator validator = new SignatureValidator(credential);
+//				validator.validate(signature);
+//				
+//				break;
+//			}
+//		}
+//
 
-
 		
-		System.out.println(client.request());
+		Element res = client.getToken();
+		
+		System.out.println(res);
+		
+		Assertion rstrAssertion = (Assertion) SAMLUtil.unmarshallElementFromString(XMLHelper.nodeToString(res));
+		OIOAssertion rstr = new OIOAssertion(rstrAssertion);
+		
+		KeyInfo ki = (KeyInfo) rstrAssertion.getSubject().getSubjectConfirmations().get(0).getSubjectConfirmationData().getUnknownXMLObjects(KeyInfo.DEFAULT_ELEMENT_NAME).get(0);
+		X509Certificate cert = ki.getX509Datas().get(0).getX509Certificates().get(0);
+		assertNotNull(cert);
+		assertTrue(Arrays.equals(credential.getEntityCertificate().getEncoded(), Base64.decode(cert.getValue())));
+		
+		assertTrue(rstr.verifySignature(Utils.getCredential("/home/recht/download/TestVOCES1.pfx", "Test1234").getPublicKey()));
 	}
 }
