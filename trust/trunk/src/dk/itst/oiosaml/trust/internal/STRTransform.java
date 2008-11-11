42a9ae5e8b7cdb7786a2eb242292d8563cb786c6
jre <jre@email.com>
2008-11-11 12:11:30 +0000

    fix transform

--- trust/trunk/src/dk/itst/oiosaml/trust/internal/STRTransform.java
+++ trust/trunk/src/dk/itst/oiosaml/trust/internal/STRTransform.java
@@ -17,17 +17,17 @@
 
 package dk.itst.oiosaml.trust.internal;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 
 import javax.xml.crypto.dsig.XMLSignature;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.apache.log4j.Logger;
 import org.apache.xml.security.c14n.CanonicalizationException;
 import org.apache.xml.security.c14n.Canonicalizer;
 import org.apache.xml.security.c14n.InvalidCanonicalizerException;
 import org.apache.xml.security.signature.XMLSignatureInput;
+import org.apache.xml.security.transforms.Transform;
 import org.apache.xml.security.transforms.TransformSpi;
 import org.apache.xml.security.utils.XMLUtils;
 import org.opensaml.ws.wssecurity.SecurityTokenReference;
@@ -46,209 +46,140 @@ import org.w3c.dom.Element;
  */
 public class STRTransform extends TransformSpi {
 
-    /**
-     * Field implementedTransformURI
-     */
-    public static final String implementedTransformURI = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-soap-message-security-1.0#STR-Transform";
+	/**
+	 * Field implementedTransformURI
+	 */
+	public static final String implementedTransformURI = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-soap-message-security-1.0#STR-Transform";
 
-    private static Log log = LogFactory.getLog(STRTransform.class.getName());
+	private static Logger log = Logger.getLogger(STRTransform.class);
 
-    private static boolean doDebug = false;
+	private static boolean doDebug = false;
 
-    private static String XMLNS = "xmlns=";
+	/**
+	 * Method engineGetURI
+	 */
+	protected String engineGetURI() {
+		return STRTransform.implementedTransformURI;
+	}
 
-    public boolean wantsOctetStream() {
-        return false;
-    }
+	/**
+	 * Method enginePerformTransform
+	 * 
+	 * @param input
+	 * @throws CanonicalizationException
+	 * @throws InvalidCanonicalizerException
+	 */
+	protected XMLSignatureInput enginePerformTransform(XMLSignatureInput input, OutputStream os, Transform _transformObject) throws IOException, CanonicalizationException, InvalidCanonicalizerException {
 
-    public boolean wantsNodeSet() {
-        return true;
-    }
+		doDebug = log.isDebugEnabled();
 
-    public boolean returnsOctetStream() {
-        return true;
-    }
+		if (doDebug) {
+			log.debug("Beginning STRTransform..." + input.toString());
+		}
 
-    public boolean returnsNodeSet() {
-        return false;
-    }
+		/*
+		 * Get the main document, that is the complete SOAP request document
+		 */
+		Document thisDoc = _transformObject.getDocument();
+		int docHash = thisDoc.hashCode();
+		if (doDebug) {
+			log.debug("doc: " + thisDoc.toString() + ", " + docHash);
+		}
 
-    /**
-     * Method engineGetURI
-     */
-    protected String engineGetURI() {
-        return STRTransform.implementedTransformURI;
-    }
+		/*
+		 * According to the OASIS WS Specification "Web Services Security:
+		 * SOAP Message Security 1.0" Monday, 19 January 2004, chapter 8.3
+		 * describes that the input node set must be processed bythe c14n
+		 * that is specified in the argument element of the STRTransform
+		 * element.
+		 * 
+		 * First step: Get the required c14n argument and get the specified
+		 * Canonicalizer
+		 */
 
-    /**
-     * Method enginePerformTransform
-     * 
-     * @param input
-     * @throws CanonicalizationException
-     * @throws InvalidCanonicalizerException
-     */
-    @SuppressWarnings("deprecation")
-	protected XMLSignatureInput enginePerformTransform(XMLSignatureInput input)
-            throws IOException, CanonicalizationException,
-            InvalidCanonicalizerException {
-
-        doDebug = log.isDebugEnabled();
-
-        if (doDebug) {
-            log.debug("Beginning STRTransform..." + input.toString());
-        }
-
-            /*
-             * Get the main document, that is the complete SOAP request document
-             */
-            Document thisDoc = this._transformObject.getDocument();
-            int docHash = thisDoc.hashCode();
-            if (doDebug) {
-                log.debug("doc: " + thisDoc.toString() + ", " + docHash);
-            }
-
-            /*
-             * According to the OASIS WS Specification "Web Services Security:
-             * SOAP Message Security 1.0" Monday, 19 January 2004, chapter 8.3
-             * describes that the input node set must be processed bythe c14n
-             * that is specified in the argument element of the STRTransform
-             * element.
-             * 
-             * First step: Get the required c14n argument and get the specified
-             * Canonicalizer
-             */
-
-            String canonAlgo = null;
-            if (this._transformObject.length(WSSecurityConstants.WSSE_NS,
-                    "TransformationParameters") == 1) {
-                Element tmpE = XMLUtils.selectNode(this._transformObject
-                        .getElement().getFirstChild(), WSSecurityConstants.WSSE_NS,
-                        "TransformationParameters", 0);
-                Element canonElem = (Element) XMLHelper.getChildElementsByTagNameNS(tmpE, XMLSignature.XMLNS, "CanonicalizationMethod").get(0); 
-                canonAlgo = canonElem.getAttribute("Algorithm");
-                if (doDebug) {
-                    log.debug("CanonAlgo: " + canonAlgo);
-                }
-            }
-            Canonicalizer canon = Canonicalizer.getInstance(canonAlgo);
-
-            ByteArrayOutputStream bos = null;
-            byte[] buf = null;
-            if (doDebug) {
-                buf = input.getBytes();
-                bos = new ByteArrayOutputStream(buf.length);
-                bos.write(buf, 0, buf.length);
-                log.debug("canon bos: " + bos.toString());
-            }
-
-            /*
-             * Get the input (node) to transform. Currently we support only an
-             * Element as input format. If other formats are required we must
-             * get it as bytes and probably reparse it into a DOM tree (How to
-             * work with nodesets? how to select the right node from a nodeset?)
-             */
-            Element str = null;
-            if (input.isElement()) {
-                str = (Element) input.getSubNode();
-            } else {
-                throw (new CanonicalizationException(
-                        "Wrong input format - only element input supported"));
-            }
-
-            if (doDebug) {
-                log.debug("STR: " + str.toString());
-            }
-            /*
-             * The element to transform MUST be a SecurityTokenReference
-             * element.
-             */
-            SecurityTokenReference secRef;
-			try {
-				secRef = (SecurityTokenReference) new SecurityTokenReferenceUnmarshaller().unmarshall(str);
-			} catch (UnmarshallingException e) {
-				throw new CanonicalizationException("Unable to handle token reference", e);
-			}
+		String canonAlgo = null;
+		if (_transformObject.length(WSSecurityConstants.WSSE_NS,
+		"TransformationParameters") == 1) {
+			Element tmpE = XMLUtils.selectNode(_transformObject
+					.getElement().getFirstChild(), WSSecurityConstants.WSSE_NS,
+					"TransformationParameters", 0);
+			Element canonElem = (Element) XMLHelper.getChildElementsByTagNameNS(tmpE, XMLSignature.XMLNS, "CanonicalizationMethod").get(0); 
+			canonAlgo = canonElem.getAttribute("Algorithm");
 			if (doDebug) {
-				log.debug("STR: " + secRef);
+				log.debug("CanonAlgo: " + canonAlgo);
 			}
-            /*
-             * Third and forth step are performed by derefenceSTR()
-             */
-            Element dereferencedToken = dereferenceSTR(thisDoc, secRef);
-            /*
-             * C14n with specified algorithm. According to WSS Specification.
-             */
-            buf = canon.canonicalizeSubtree(dereferencedToken, "#default");
-            if (doDebug) {
-                bos = new ByteArrayOutputStream(buf.length);
-                bos.write(buf, 0, buf.length);
-                log.debug("after c14n: " + bos.toString());
-            }
+		}
+		Canonicalizer canon = Canonicalizer.getInstance(canonAlgo);
 
-            if (true) 
-            return new XMLSignatureInput(buf);
-            /*
-             * Alert: Hacks ahead According to WSS spec an Apex node must
-             * contain a default namespace. If none is availabe in the first
-             * node of the c14n output (this is the apex element) then we do
-             * some editing to insert an empty default namespace
-             * 
-             * TODO: Rework theses hacks after c14n was updated and can be
-             * instructed to insert empty default namespace if required
-             */
-            // If the problem with c14n method is solved then just do:
-            
-            // start of HACK
-            StringBuffer bf = new StringBuffer(new String(buf));
-            String bf1 = bf.toString();
+		if (doDebug) {
+			log.debug("canon bos: " + new String(input.getBytes()));
+		}
 
-            /*
-             * Find start and end of first element <....>, this is the Apex node
-             */
-            int gt = bf1.indexOf(">");
-            /*
-             * Lookup the default namespace
-             */
-            int idx = bf1.indexOf(XMLNS);
-            /*
-             * If none found or if it is outside of this (Apex) element look for
-             * first blank in, insert default namespace there (this is the
-             * correct place according to c14n specification)
-             */
-            if (idx < 0 || idx > gt) {
-                idx = bf1.indexOf(" ");
-                bf.insert(idx + 1, "xmlns=\"\" ");
-                bf1 = bf.toString();
-            }
-            if (doDebug) {
-                log.debug("last result: " + bf1);
-            }
-            return new XMLSignatureInput(bf1.getBytes());
-        // End of HACK
-    }
+		/*
+		 * Get the input (node) to transform. Currently we support only an
+		 * Element as input format. If other formats are required we must
+		 * get it as bytes and probably reparse it into a DOM tree (How to
+		 * work with nodesets? how to select the right node from a nodeset?)
+		 */
+		Element str = null;
+		if (input.isElement()) {
+			str = (Element) input.getSubNode();
+		} else {
+			throw (new CanonicalizationException(
+					"Wrong input format - only element input supported"));
+		}
 
-    private Element dereferenceSTR(Document doc, SecurityTokenReference secRef) throws CanonicalizationException {
+		if (doDebug) {
+			log.debug("STR: " + str);
+		}
+		/*
+		 * The element to transform MUST be a SecurityTokenReference
+		 * element.
+		 */
+		SecurityTokenReference secRef;
+		try {
+			secRef = (SecurityTokenReference) new SecurityTokenReferenceUnmarshaller().unmarshall(str);
+		} catch (UnmarshallingException e) {
+			throw new CanonicalizationException("Unable to handle token reference", e);
+		}
+		if (doDebug) {
+			log.debug("STR: " + secRef);
+		}
+		/*
+		 * Third and forth step are performed by derefenceSTR()
+		 */
+		Element dereferencedToken = dereferenceSTR(thisDoc, secRef);
+		/*
+		 * C14n with specified algorithm. According to WSS Specification.
+		 */
+		if (os != null) {
+			canon.setWriter(os);
+		}
+		byte[] buf = canon.canonicalizeSubtree(dereferencedToken);
 
-        /*
-         * First case: direct reference, according to chap 7.2 of OASIS WS
-         * specification (main document). Only in this case return a true
-         * reference to the BST. Copying is done by the caller.
-         */
-    	String id = null;
-    	if (secRef.getKeyIdentifier() != null) {
-        	id = secRef.getKeyIdentifier().getValue();
-        } else if (secRef.getReference() != null) {
-        	id = secRef.getReference().getURI().substring(1);
-        }
-    	Element token = doc.getElementById(id);
-    	if (doDebug) {
-    		log.debug("Referenced token: " + token);
-    	}
-    	if (token == null) {
-    		throw new CanonicalizationException("Token with id " + id + " not found");
-    	}
+		XMLSignatureInput result = new XMLSignatureInput(buf);
+		if (os != null) {
+			input.setOutputStream(os);
+		}
+		return result;
+	}
+
+	private Element dereferenceSTR(Document doc, SecurityTokenReference secRef) throws CanonicalizationException {
+		String id = null;
+		if (secRef.getKeyIdentifier() != null) {
+			id = secRef.getKeyIdentifier().getValue();
+		} else if (secRef.getReference() != null) {
+			id = secRef.getReference().getURI().substring(1);
+		}
+		Element token = doc.getElementById(id);
+		if (doDebug) {
+			log.debug("Referenced token: " + token);
+		}
+		if (token == null) {
+			throw new CanonicalizationException("Token with id " + id + " not found");
+		}
 		return token;
-        
-    }
+
+	}
 
 }
