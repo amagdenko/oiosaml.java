2228e3594286265975ea1c31dbea99ff57804433
jre <jre@email.com>
2008-12-17 09:30:17 +0000

    throw a InvalidSecurity fault on validation errors

--- poc/provider/src/dk/itst/saml/poc/provider/Provider.java
+++ poc/provider/src/dk/itst/saml/poc/provider/Provider.java
@@ -8,21 +8,28 @@ import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
 import javax.security.auth.Subject;
 import javax.servlet.http.HttpServletRequest;
+import javax.xml.namespace.QName;
+import javax.xml.soap.SOAPException;
+import javax.xml.soap.SOAPFactory;
 import javax.xml.stream.XMLStreamReader;
 import javax.xml.ws.Endpoint;
 import javax.xml.ws.WebServiceContext;
 import javax.xml.ws.handler.MessageContext;
+import javax.xml.ws.soap.SOAPFaultException;
 
 import org.apache.axiom.om.impl.builder.StAXOMBuilder;
 import org.apache.log4j.Logger;
 import org.opensaml.saml2.core.Assertion;
+import org.opensaml.ws.wssecurity.WSSecurityConstants;
 import org.opensaml.xml.XMLObject;
 
 import com.sun.xml.wss.SubjectAccessor;
+import com.sun.xml.wss.XWSSecurityException;
 
 import dk.itst.oiosaml.common.SAMLUtil;
 import dk.itst.oiosaml.sp.model.OIOAssertion;
 import dk.itst.oiosaml.sp.model.validation.BasicAssertionValidator;
+import dk.itst.oiosaml.sp.model.validation.ValidationException;
 import dk.itst.saml.poc.idws.Framework;
 import dk.itst.saml.poc.idws.FrameworkMismatchFault;
 import dk.itst.saml.poc.idws.RequestToInteractFault;
@@ -48,15 +55,21 @@ public class Provider {
 			
 			OIOAssertion assertion = new OIOAssertion(getCredential(subject));
 			HttpServletRequest req = (HttpServletRequest) context.getMessageContext().get(MessageContext.SERVLET_REQUEST);
-			assertion.validateAssertion(new BasicAssertionValidator(), req.getRequestURL().toString(), req.getRequestURL().toString());
+			try {
+				assertion.validateAssertion(new BasicAssertionValidator(), req.getRequestURL().toString(), req.getRequestURL().toString());
+			} catch (ValidationException e) {
+				throw new SOAPFaultException(SOAPFactory.newInstance().createFault(e.getMessage(), new QName(WSSecurityConstants.WSSE_NS, "InvalidSecurity")));
+			}
 			
 			return input;
 		} catch (FrameworkMismatchFault e) {
 			throw e;
-		} catch (Exception e) {
+		} catch (XWSSecurityException e) {
 			e.printStackTrace();
 			throw new RuntimeException(e);
-		}
+		} catch (SOAPException e) {
+			throw new RuntimeException(e);
+		} 
 	}
 	
 	private Assertion getCredential(Subject subject) {
@@ -101,46 +114,6 @@ public class Provider {
 			return info;
 		}
 	}
-
-	/*
-	private void validateAssertion(Subject subject, StringBuffer sb)
-			throws ParserConfigurationException,
-			TransformerFactoryConfigurationError,
-			TransformerConfigurationException, TransformerException,
-			MalformedURLException, IOException {
-		XMLStreamReader r = (XMLStreamReader) subject.getPublicCredentials().iterator().next();
-		DocumentBuilderFactory df = DocumentBuilderFactory.newInstance();
-		df.setNamespaceAware(true);
-		DocumentBuilder builder = df.newDocumentBuilder();
-		Document doc = builder.newDocument();
-		
-		TransformerFactory tf = TransformerFactory.newInstance();
-		Transformer t = tf.newTransformer();
-		t.transform(new StAXSource(r, true),new DOMResult(doc));
-		
-		STSClient client = new STSClient("http://tri-test1.trifork.com:8080/sts/");
-		client.registerSecurityTokenReference(new QName("urn:oasis:names:tc:SAML:2.0:assertion", "Assertion"), KeyIdentifierTokenReference.SAML20_TOKEN_TYPE, 
-				new KeyIdentifierTokenReference(KeyIdentifierTokenReference.SAML20_TOKEN_TYPE, KeyIdentifierTokenReference.SAML20_VALUE_TYPE, new IdProvider() {
-					public String obtainId(Element e) throws STSClientException {
-						log.debug("Resolving id for " + e);
-						return e.getAttribute("ID");
-					}
-				}));
-
-		BasicX509Credential credential = Utils.getCredential("/home/recht/download/TestVOCES1.pfx", "Test1234");
-
-		RequestSecurityTokenData vd = client.createValidateData();
-		vd.setTokenType(KeyIdentifierTokenReference.SAML20_TOKEN_TYPE);
-		STSResponse response = client.makeRequest(vd, doc.getDocumentElement(), null, KeyIdentifierTokenReference.SAML20_TOKEN_TYPE, new X500PrivateCredential(credential.getEntityCertificate(), credential.getPrivateKey()));
-		
-		sb.append("Validate response: ").append(response).append("\n");
-		sb.append("Fail: ").append(response.isFault()).append("\n");
-		if (!response.isFault()) {
-			sb.append("Message: ").append(Utils.beautifyAndHtmlXML(XMLHelper.nodeToString(response.getStsMessage().toDocument().getDocumentElement()), "&nbsp;&nbsp;&nbsp;&nbsp;")).append("\n");
-		} else {
-			sb.append("Fail message: ").append(response.getSoapFault().getDetail()).append("\n");
-		}
-	}*/
 
 	public static void main(String[] args) {
 		Endpoint.publish("http://recht-laptop:8880/poc-provider/ProviderService", new Provider());
