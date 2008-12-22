2adcbab41808edcdd1504beb6b807ae6fccabd2e
jre <jre@email.com>
2008-12-22 14:22:14 +0000

    do not use oioassertio signing

--- poc/sts/src/dk/itst/saml/sts/TokenService.java
+++ poc/sts/src/dk/itst/saml/sts/TokenService.java
@@ -1,12 +1,35 @@
 package dk.itst.saml.sts;
 
 import java.io.IOException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.XMLConstants;
+import javax.xml.crypto.MarshalException;
+import javax.xml.crypto.dsig.CanonicalizationMethod;
+import javax.xml.crypto.dsig.DigestMethod;
+import javax.xml.crypto.dsig.Reference;
+import javax.xml.crypto.dsig.SignatureMethod;
+import javax.xml.crypto.dsig.SignedInfo;
+import javax.xml.crypto.dsig.Transform;
+import javax.xml.crypto.dsig.XMLSignature;
+import javax.xml.crypto.dsig.XMLSignatureException;
+import javax.xml.crypto.dsig.XMLSignatureFactory;
+import javax.xml.crypto.dsig.dom.DOMSignContext;
+import javax.xml.crypto.dsig.keyinfo.KeyInfoFactory;
+import javax.xml.crypto.dsig.spec.C14NMethodParameterSpec;
+import javax.xml.crypto.dsig.spec.ExcC14NParameterSpec;
+import javax.xml.crypto.dsig.spec.TransformParameterSpec;
 import javax.xml.namespace.QName;
 
 import org.apache.commons.configuration.Configuration;
@@ -14,6 +37,7 @@ import org.apache.commons.io.IOUtils;
 import org.apache.log4j.Logger;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
+import org.opensaml.common.xml.SAMLConstants;
 import org.opensaml.saml2.core.Assertion;
 import org.opensaml.saml2.core.Subject;
 import org.opensaml.saml2.core.SubjectConfirmation;
@@ -28,6 +52,7 @@ import org.opensaml.ws.wssecurity.Expires;
 import org.opensaml.ws.wssecurity.KeyIdentifier;
 import org.opensaml.ws.wssecurity.Security;
 import org.opensaml.ws.wssecurity.SecurityTokenReference;
+import org.opensaml.ws.wssecurity.WSSecurityConstants;
 import org.opensaml.ws.wstrust.Lifetime;
 import org.opensaml.ws.wstrust.RequestSecurityToken;
 import org.opensaml.ws.wstrust.RequestSecurityTokenResponse;
@@ -36,11 +61,17 @@ import org.opensaml.ws.wstrust.RequestedAttachedReference;
 import org.opensaml.ws.wstrust.RequestedSecurityToken;
 import org.opensaml.ws.wstrust.RequestedUnattachedReference;
 import org.opensaml.ws.wstrust.TokenType;
+import org.opensaml.xml.Namespace;
+import org.opensaml.xml.XMLObject;
 import org.opensaml.xml.security.x509.BasicX509Credential;
+import org.opensaml.xml.security.x509.X509Credential;
 import org.opensaml.xml.signature.KeyInfo;
 import org.opensaml.xml.signature.X509Certificate;
 import org.opensaml.xml.signature.X509Data;
 import org.opensaml.xml.util.XMLHelper;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 import dk.itst.oiosaml.common.OIOSAMLConstants;
 import dk.itst.oiosaml.common.SAMLUtil;
@@ -53,6 +84,7 @@ import dk.itst.oiosaml.trust.OIOSoapEnvelope;
 import dk.itst.oiosaml.trust.SigningPolicy;
 import dk.itst.oiosaml.trust.TrustBootstrap;
 import dk.itst.oiosaml.trust.TrustConstants;
+import dk.itst.oiosaml.trust.internal.SignatureFactory;
 
 public class TokenService extends HttpServlet {
 	private static CredentialRepository credentialRepository = new CredentialRepository();
@@ -194,7 +226,7 @@ public class TokenService extends HttpServlet {
 		confirmation.getNameID().setFormat(NameIDFormat.ENTITY.getFormat());
 		
 		SubjectConfirmationData data = SAMLUtil.buildXMLObject(SubjectConfirmationData.class);
-		data.getUnknownAttributes().put(new QName("http://www.w3.org/2001/XMLSchema-instance", "type"), "saml:KeyInfoConfirmationDataType");
+		data.getUnknownAttributes().put(new QName("http://www.w3.org/2001/XMLSchema-instance", "type", "xsi"), "saml:KeyInfoConfirmationDataType");
 		
 		KeyInfo keyInfo = SAMLUtil.buildXMLObject(KeyInfo.class);
 		X509Data x509data = SAMLUtil.buildXMLObject(X509Data.class);
@@ -215,11 +247,96 @@ public class TokenService extends HttpServlet {
 		if (bootstrap != null) {
 			a.getAttributeStatements().add(SAMLUtil.clone(bootstrap.getAssertion().getAttributeStatements().get(0)));
 		}
+		a.addNamespace(new Namespace(XMLConstants.W3C_XML_SCHEMA_NS_URI, "xs"));
+		a.addNamespace(new Namespace(XMLConstants.W3C_XML_SCHEMA_INSTANCE_NS_URI, "xsi"));
 		
-		OIOAssertion oa = new OIOAssertion(SAMLUtil.clone(a));
-		oa.sign(credential);
+//		OIOAssertion oa = new OIOAssertion(SAMLUtil.clone(a));
+//		oa.sign(credential);
+//		
+//		return (Assertion) SAMLUtil.unmarshallElementFromString(oa.toXML());
 		
-		return oa.getAssertion();
+		try {
+			Element signed = sign(a, credential);
+			log.debug("Signed assertion: " + XMLHelper.nodeToString(signed));
+			return (Assertion) SAMLUtil.unmarshallElement(signed);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 	}
 
+	
+	public Element sign(Assertion assertion, X509Credential credential) throws NoSuchAlgorithmException, InvalidAlgorithmParameterException, MarshalException, XMLSignatureException {
+		Map<XMLObject, String> references = new HashMap<XMLObject, String>();
+		references.put(assertion, assertion.getID());
+		
+		XMLSignatureFactory xsf = SignatureFactory.getInstance();
+		
+		CanonicalizationMethod canonicalizationMethod = xsf.newCanonicalizationMethod(CanonicalizationMethod.EXCLUSIVE, (C14NMethodParameterSpec) null);
+		SignatureMethod signatureMethod = xsf.newSignatureMethod(SignatureMethod.RSA_SHA1, null);
+
+		KeyInfoFactory keyInfoFactory = xsf.getKeyInfoFactory();
+		javax.xml.crypto.dsig.keyinfo.KeyInfo ki = keyInfoFactory.newKeyInfo(Collections.singletonList(keyInfoFactory.newX509Data(Collections.singletonList(credential.getEntityCertificate()))));
+    	
+    	List<Reference> refs = new ArrayList<Reference>();
+		
+		DigestMethod digestMethod = xsf.newDigestMethod(DigestMethod.SHA1, null);
+		List<Transform> transforms = new ArrayList<Transform>(2);
+		transforms.add(xsf.newTransform("http://www.w3.org/2000/09/xmldsig#enveloped-signature", (TransformParameterSpec)null));
+		transforms.add(xsf.newTransform("http://www.w3.org/2001/10/xml-exc-c14n#",(ExcC14NParameterSpec)null));
+
+		for (Map.Entry<XMLObject, String> ref : references.entrySet()) {
+			Reference r = xsf.newReference("#"+ref.getValue(), digestMethod, transforms, null, null);
+			refs.add(r);
+		}
+
+		
+		SAMLUtil.marshallObject(assertion);
+		
+		// Create the SignedInfo
+		SignedInfo signedInfo = xsf.newSignedInfo(canonicalizationMethod, signatureMethod, refs);
+        
+        
+        String signatureId = Utils.generateUUID();
+		XMLSignature signature = xsf.newXMLSignature(signedInfo, ki, null, signatureId, null);
+        
+        String xml = XMLHelper.nodeToString(assertion.getDOM());
+        log.debug("Signing envelope: " + xml);
+        Element element = SAMLUtil.loadElementFromString(xml);
+        
+        Node next = element.getElementsByTagNameNS(Subject.DEFAULT_ELEMENT_NAME.getNamespaceURI(), Subject.DEFAULT_ELEMENT_LOCAL_NAME).item(0);
+        
+        DOMSignContext signContext = new DOMSignContext(credential.getPrivateKey(), element, next); 
+        signContext.putNamespacePrefix(SAMLConstants.XMLSIG_NS, SAMLConstants.XMLSIG_PREFIX);
+        signContext.putNamespacePrefix(SAMLConstants.XMLENC_NS, SAMLConstants.XMLENC_PREFIX);
+
+        for (XMLObject o : references.keySet()) {
+        	fixIdAttributes(element, o);
+        }
+        
+        // Marshal, generate (and sign) the detached XMLSignature. The DOM
+        // Document will contain the XML Signature if this method returns
+        // successfully.
+        // HIERARCHY_REQUEST_ERR: Raised if this node is of a type that does not allow children of the type of the newChild  node, or if the node to insert is one of this node's ancestors.
+        signature.sign(signContext);
+
+        return element;
+	}
+
+	private void fixIdAttributes(Element env, XMLObject obj) {
+		if (obj == null) return;
+		
+		if (log.isDebugEnabled()) log.debug("Fixing id attribute on " + obj);
+		
+    	NodeList nl = env.getElementsByTagNameNS(obj.getDOM().getNamespaceURI(), obj.getDOM().getLocalName());
+    	for (int i = 0; i < nl.getLength(); i++) {
+    		Element e = (Element) nl.item(i);
+    		if (e.hasAttribute("ID")) {
+    			e.setIdAttributeNS(null, "ID", true);
+    		}
+    		if (e.hasAttributeNS(WSSecurityConstants.WSU_NS, "Id")) {
+    			e.setIdAttributeNS(WSSecurityConstants.WSU_NS, "Id", true);
+    		}
+    	}
+		
+	}
 }
