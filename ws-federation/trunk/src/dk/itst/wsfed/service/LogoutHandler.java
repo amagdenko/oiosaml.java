07459f72776eb0fce12ea972f0b05469578f4b05
jre <jre@email.com>
2009-06-03 10:06:32 +0000

    added LogoutHandler for WS-Fed

---
+++ ws-federation/trunk/src/dk/itst/wsfed/service/LogoutHandler.java
@@ -0,0 +1,20 @@
+package dk.itst.wsfed.service;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+
+import dk.itst.oiosaml.sp.service.RequestContext;
+import dk.itst.oiosaml.sp.service.SAMLHandler;
+
+public class LogoutHandler implements SAMLHandler {
+
+	public void handleGet(RequestContext arg0) throws ServletException, IOException {
+		
+	}
+
+	public void handlePost(RequestContext arg0) throws ServletException, IOException {
+		
+	}
+
+}
