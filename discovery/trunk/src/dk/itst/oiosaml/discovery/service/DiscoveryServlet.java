8a436fe89cb9db8b17014453fbde8226fa7af775
jre <jre@email.com>
2009-12-04 10:50:16 +0000

    added servlets for setting cdc cookie and clearing it

--- discovery/trunk/src/dk/itst/oiosaml/discovery/service/DiscoveryServlet.java
+++ discovery/trunk/src/dk/itst/oiosaml/discovery/service/DiscoveryServlet.java
@@ -24,22 +24,17 @@
 package dk.itst.oiosaml.discovery.service;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.Arrays;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
 
-public class DiscoveryServlet extends HttpServlet {
+public class DiscoveryServlet extends AbstractServlet {
 	private static final Logger log = Logger.getLogger(DiscoveryServlet.class);
-	
-	public static final String REFERER_PARAMETER = "r";
-	public static final String SAML_IDP_COOKIE = "_saml_idp";
 	
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		log.info("Discovery request from " + req.getRemoteAddr() + ", referer: " + req.getParameter(REFERER_PARAMETER));
@@ -56,35 +51,12 @@ public class DiscoveryServlet extends HttpServlet {
 			log.info("No saml idp cookie found, redirecting with empty parameter");
 			sendRedirect(req.getParameter(REFERER_PARAMETER), "", resp);
 		} else {
-			sendRedirect(req.getParameter(REFERER_PARAMETER), samlIdpCookie.getValue(), resp);
+			sendRedirect(req.getParameter(REFERER_PARAMETER), Base64.encodeBytes(samlIdpCookie.getValue().getBytes()), resp);
 		}
 	}
 	
-	private void sendRedirect(String url, String cookie, HttpServletResponse res) throws IOException {
-		res.setContentType("text/html");
-		
-		PrintWriter w = res.getWriter();
-		w.write("<html><head>");
-		w.write("<meta http-equiv=\"refresh\" content=\"0;url=");
-		w.write(url);
-		if (url.indexOf('?') > -1) {
-			w.write("&_saml_idp=");
-		} else {
-			w.write("?_saml_idp=");
-		}
-		w.write(cookie);
-		w.write("\">");
-		w.write("</head><body></body></html>");
-	}
-	
-	private Cookie findCookie(Cookie[] cookies) {
-		if (cookies == null) return null;
-		
-		for (Cookie cookie : cookies) {
-			if (SAML_IDP_COOKIE.equalsIgnoreCase(cookie.getName())) {
-				return cookie;
-			}
-		}
-		return null;
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doGet(req, resp);
 	}
 }
